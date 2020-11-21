872603d379af701a49ac00527ae430a222a1e248
Leonardo Amorim <leonardo123.amorim@gmail.com>
2020-11-21 16:46:42 -0300

    init

---
+++ novo.js
@@ -0,0 +1,117 @@
+function generateInitialState() {
+    misturar = () => Math.floor(Math.random() * 3);
+    const array = [
+        [1, 2, 3].sort(misturar), [4, 5, 6].sort(misturar), [7, 8, " "].sort(misturar),
+    ].sort(misturar);
+    return array;
+}
+
+class TreeNode {
+    constructor(state, parent = null) {
+        this.state = state;
+        this.parent = parent;
+        this.descendents = [];
+    }
+
+    getPosWhiteSpace() {
+        for (let i = 0; i < this.state.length; i++)
+            for (let j = 0; j < this.state[i].length; j++)
+                if (this.state[i][j] == " ") return [i, j];
+    }
+
+    hash() {
+            return this.state.reduce(
+                    (acc1, linha) =>
+                    `${acc1}${linha.reduce((acc2, pos) => `${acc2}${pos}`, "")}`,
+      ""
+    );
+  }
+  swap(posAI, posAJ, posBI, posBJ) {
+    const copyState = this.state.map(function (arr) {
+      return arr.slice();
+    });
+    copyState[posAI][posAJ] = copyState[posBI][posBJ];
+    copyState[posBI][posBJ] = " ";
+
+    return copyState;
+  }
+  reducer(action) {
+    const [posI, posJ] = this.getPosWhiteSpace();
+    switch (action) {
+      case "UP":
+        if (posI + 1 < 3) return this.swap(posI, posJ, posI + 1, posJ);
+        break;
+      case "DOWN":
+        if (posI - 1 >= 0) return this.swap(posI, posJ, posI - 1, posJ);
+        break;
+      case "LEFT":
+        if (posJ + 1 < 3) return this.swap(posI, posJ, posI, posJ + 1);
+        break;
+      case "RIGTH":
+        if (posJ - 1 >= 0) return this.swap(posI, posJ, posI, posJ - 1);
+        break;
+      default:
+        return state;
+    }
+    return false;
+  }
+
+  distance() {
+    let pos = 0;
+    let distancia = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        pos++;
+        if (this.state[i][j] != " " && this.state[i][j] != pos) {
+          const linha = Math.ceil(this.state[i][j] / 3) - 1;
+          let coluna = (this.state[i][j] % 3) - 1;
+          if (coluna < 0) coluna = 2;
+          distancia += Math.abs(linha - i) + Math.abs(coluna - j);
+        }
+      }
+    }
+    return distancia;
+  }
+
+  expand() {
+    const actions = ["UP", "DOWN", "LEFT", "RIGTH"];
+    const newDescendents = [];
+    actions.forEach((action) => {
+      const newState = this.reducer(action);
+      if (newState) {
+        const node = new TreeNode(newState, this);
+        newDescendents.push(node);
+      }
+    });
+
+    this.descendents = newDescendents;
+  }
+
+  isSolution() {
+    const hash = this.hash().replace(" ", "");
+    return hash === "12345678";
+  }
+}
+
+function getSolution() {
+  const root = new TreeNode(generateInitialState());
+  const frontier = [root];
+  const hashes = [];
+  while (frontier.length > 0) {
+    const element = frontier.shift();
+    console.log(element.state);
+
+    if (element.isSolution()) return element;
+    const hash = element.hash();
+
+    if (!hashes.includes(hash)) {
+      console.log("Hash: ", hash);
+      hashes.push(hash);
+      element.expand();
+
+      element.descendents.forEach((descendent) => frontier.push(descendent));
+    }
+  }
+}
+const solution = getSolution();
+console.log(solution);
